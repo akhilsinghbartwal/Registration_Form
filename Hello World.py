b1a56b886cae3200a3c3e673a01d325caf071f74
Akhil <69353570+Akhil126@users.noreply.github.com>
2020-08-16 18:50:03 +0530

    Update and rename python.py to Hello World.py

--- Hello World.py
+++ Hello World.py
@@ -2,4 +2,4 @@ print("Hello World")
 # This code is use to print the letter inside ("") this
 
 a = input()
-# This code is use to freeze cmd window during run code.
+# This code is use to freeze cmd or powershell window during run code.
